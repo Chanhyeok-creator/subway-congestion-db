c1a705c71ac5abe25bb47c09f98566319758650c
Chanhyeok-creator <k89474075@gmail.com>
2025-12-14 18:06:50 +0900

    flask 서버 기본 뼈대 만들기

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template,request, redirect, url_for
+import sqlite3
+
+DB_PATH = "database.db"
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
